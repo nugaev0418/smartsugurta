45cbeeba8943ef80cc1eb853f8f19b4f81ee322b
Nugaev <sardor0418@mail.ru>
2026-01-30 15:15:07 +0500

    insert season migration

--- console/migrations/m260130_100615_create_insert_season.php
+++ console/migrations/m260130_100615_create_insert_season.php
@@ -9,7 +9,7 @@ class m260130_100615_create_insert_season extends Migration
      */
     public function safeUp()
     {
-        $this->execute("INSERT INTO `seasonalInsurance` (`id`, `name`, `days`, `seasonId`) VALUES
+        $this->execute("INSERT INTO `season` (`id`, `name`, `days`, `seasonId`) VALUES
 (1, '1 yil', 365, '8465a831-850f-4445-a995-ef71195094ab'),
 (2, '6 oy', 180, '9848096e-cc12-4dbd-893b-41f2cdfc9a0e'),
 (3, '20 kun', 20, '0d546748-0ba6-43bc-9ce2-1b977ad9e494');");
