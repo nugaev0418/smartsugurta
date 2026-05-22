2df3ee6b0ae235c804b0d0b6d8fa920ca70bbc12
Nugaev <sardor0418@mail.ru>
2026-05-22 16:56:57 +0500

    123

--- backend/gross/GrossOsago.php
+++ backend/gross/GrossOsago.php
@@ -83,26 +83,26 @@ class GrossOsago
         print_r($contractData);
 
 
-        $contractResp = $this->call('contract',
-            fn() => $this->http->createContract($contractData),
-            $sessionDir
-        );
+//        $contractResp = $this->call('contract',
+//            fn() => $this->http->createContract($contractData),
+//            $sessionDir
+//        );
+//
+//
+//        print_r($contractResp);
+//
+//
+//        if (!$contractResp) throw new RuntimeException("Shartnoma yaratilmadi");
+//
+//        $uuid     = $contractResp['uuid']       ?? ($contractResp['data']['uuid'] ?? null);
+//        $anketaId = (string) ($contractResp['anketa_id'] ?? '');
+//        $premium  = (int) ($contractResp['premium'] ?? 0);
 
 
-        print_r($contractResp);
 
-
-        if (!$contractResp) throw new RuntimeException("Shartnoma yaratilmadi");
-
-        $uuid     = $contractResp['uuid']       ?? ($contractResp['data']['uuid'] ?? null);
-        $anketaId = (string) ($contractResp['anketa_id'] ?? '');
-        $premium  = (int) ($contractResp['premium'] ?? 0);
-
-
-
-//        $uuid     = 'b50f1184-32dd-40dc-9020-60ed187cb540';
-//        $anketaId = '5647130';
-//        $premium  = '56000';
+        $uuid     = 'b50f1184-32dd-40dc-9020-60ed187cb540';
+        $anketaId = '5647130';
+        $premium  = '56000';
 
 
         $clickHtml = $this->http->payWithClick($uuid, $anketaId);
